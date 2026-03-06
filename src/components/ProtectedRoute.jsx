62cb9d7f919646e8fad8d1dff9613e42b4bd6fc3
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-06 08:48:16 +0200

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+
+  const user = JSON.parse(localStorage.getItem("adminUser"));
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
