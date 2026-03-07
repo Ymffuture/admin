a680fbc372df1d86395a55d550ef0374748ee2d1
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-03-07 06:24:21 +0200

    Update auth.api.js

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://kotabites.onrender.com";
+const API = "https://kotabites.onrender.com/api";
 
 export const loginAdmin = (data) =>
   axios.post(`${API}/auth/login`, data);
